dc62a00868c7163477fd0d2428384bda9e223781
Shreeya Bansal <shreeyabansal77@gmail.com>
2025-03-10 00:47:43 +0530

    migrations updated

--- job/models.py
+++ job/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 
 class StudentUser(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    mobile = models.CharField(max_length=15,null=True)
+    mobile = models.CharField(max_length=10,null=True)
     image = models.FileField(null=True)
     gender = models.CharField(max_length=10,null=True)
-    type = models. CharField(max_length=15,null=True)
+    type = models.CharField(max_length=15,null=True)
     def __str__(self):
         return self.user.username
 
@@ -25,7 +25,7 @@ class Recruiter(models.Model):
     gender = models.CharField(max_length=10,null=True)
     company = models.CharField(max_length=100,null=True)
     status = models.CharField(max_length=20, null=True)
-    type = models. CharField(max_length=15,null=True)
+    type = models.CharField(max_length=15,null=True)
     def __str__(self):
         return self.user.username
 
